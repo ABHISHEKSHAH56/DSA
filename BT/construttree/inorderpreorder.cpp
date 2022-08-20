a5f7a073f2e1711c70a80d8a4f451861fbe788bf
Abhisehk <abhishek.shah@pracbee.com>
2022-08-21 04:21:26 +0530

    construction added

---
+++ BT/construttree/inorderpreorder.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode * buildbro(vector<int>&in,vector<int>& pre, int prestart,int preend,int instart,int inend ,unordered_map<int,int> & mp)
+{
+    if(prestart>preend || instart>inend) return NULL;
+    TreeNode *root=new TreeNode(pre[prestart]);
+    int inRoot=mp[root->val];
+    int numsleft=inRoot-instart;
+    root->left=buildbro(in,pre,prestart+1,prestart+numsleft,instart,inRoot-1,mp);
+    root->right=buildbro(in,pre,prestart+numsleft+1,preend,inRoot+1,inend,mp);
+    return root;
+}
+
+//need to change
+TreeNode * buildtree(vector<int>inorder,vector<int>postorder)
+{
+    //as concept we know that 
+    //pre and post will give the idea who is root and inorder help to segrate the left and right part '
+    //for that we need indexing 
+    unordered_map<int,int>mp;
+    for(int i=0; i<inorder.size(); i++) mp[inorder[i]]=i;
+}
