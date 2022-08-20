16fad0a4c835316132ecc050d78a316d7ed9e7a2
Abhisehk <abhishek.shah@pracbee.com>
2022-08-21 03:15:32 +0530

    diagonal traversal of binary tree

--- BT/Traversal/diagnaltraversal.cpp
+++ BT/Traversal/diagnaltraversal.cpp
@@ -12,7 +12,29 @@ using namespace std;
 //for symmetrynlr===rln
 
 
-bool isdiagnaltraversal(TreeNode*root)
+vector<int> diagonal(TreeNode *root)
 {
-
+   // your code here
+   queue<TreeNode* >q;
+   q.push(root);
+   vector<int>ans;
+   while(!q.empty())
+   {
+       int size=q.size();
+      
+       for(int i=0; i<size; i++)
+       {
+           auto it =q.front();
+           q.pop();
+           
+           while(it!=NULL)
+           {
+               if(it->left) q.push(it->left);
+               ans.push_back(it->val);
+               it=it->right;
+           }
+           
+       }
+   }
+   return ans;
 }
